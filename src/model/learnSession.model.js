b89755678d6f0c4e3b8a6c67a42891fa2e3dbfc2
SiminNaghi <simin.naghi@outlook.com>
2023-11-30 18:32:31 +0100

    bug fix

--- src/model/learnSession.model.js
+++ src/model/learnSession.model.js
@@ -18,23 +18,6 @@ const LearnSessionSchema = new Schema({
       ref: "Flashcard",
     },
   ],
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  toLearn: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Flashcard",
-    },
-  ],
-  isLearned: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Flashcard",
-    },
-  ],
 });
 
 const LearnSession = model("Learnsession", LearnSessionSchema);
