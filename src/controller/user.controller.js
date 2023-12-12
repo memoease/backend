7d66cd5b77ca98ffc25838381c24b308e2867d70
djoetker <jansiede@posteo.de>
2023-12-12 11:50:23 +0100

    cookie domain

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -210,7 +210,7 @@ export async function validateToken(req, res, next) {
 export async function logoutUser(req, res, next) {
   try {
     // Delete Auth-Token-Cookie
-    res.clearCookie("authToken");
+    res.clearCookie("authToken", { domain: process.env.COOKIE_DOMAIN });
 
     // Delete User-Info-Cookie
     res.clearCookie("userInfo");
